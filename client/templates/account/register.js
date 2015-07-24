5385e74c90461e614d982b78140f6d49c6187ac8
MistakenElf <bigt371@gmail.com>
2015-07-24 10:17:53 -0500

    Fixed user registration to use a username

--- client/templates/account/register.js
+++ client/templates/account/register.js
@@ -1,13 +1,20 @@
 Template.register.events({
   'submit form': function(event){
     event.preventDefault();
-    var email = $('[name=email]').val();
+    var first_name = $('[name=first_name]').val();
+    var last_name = $('[name=last_name]').val();
+    var username = $('[name=username]').val();
     var password = $('[name=password]').val();
+    var email = $('[name=email]').val();
 
     Accounts.createUser({
-        email: email,
-        password: password
+      first_name: first_name,
+      last_name: last_name,
+      username: username,
+      password: password,
+      email: email
     },
+
     function(error) {
       if(error) {
         alert(error.reason);
