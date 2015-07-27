06dba55fa766cbf85ba6cb10c9cb261f28e845ab
MistakenElf <bigt371@gmail.com>
2015-07-27 17:24:02 -0500

    Added validation to login with autoform and simple schema

---
+++ both/collections/login_schema.js
@@ -0,0 +1,13 @@
+Schema = {};
+
+Schema.login = new SimpleSchema({
+  username: {
+    type: String,
+    regEx: /^[a-z0-9A-Z_]{3,15}$/
+    },
+
+  password: {
+    type: String,
+    regEx: /^[a-z0-9A-Z_]{3,15}$/
+    },
+});
--- client/templates/account/login/login.js
+++ client/templates/account/login/login.js
@@ -1,8 +1,9 @@
 Template.login.events({
-  'submit form': function(event) {
+  'submit form': function(event){
     event.preventDefault();
     var username = $('[name=username]').val();
     var password = $('[name=password]').val();
+
     Meteor.loginWithPassword(username, password, function(error){
       if(error) {
         alert(error.reason);
