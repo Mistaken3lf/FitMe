e11f51d6e50c0086a8b1b2122cde75c0d5fbb9df
Mistaken3lf <mistaken3lf@gmail.com>
2015-08-07 10:36:23 -0500

    Fixed routes and added a client dashboard template to display the current clients information

--- server/methods/deleteClient.js
+++ server/methods/deleteClient.js
@@ -1,5 +1,10 @@
 Meteor.methods({
   deleteClient: function (clientId) {
+
+    if(!Meteor.userId()) {
+      throw new Meteor.Error("not-authorized");
+    }
+
     var client = Meteor.users.findOne(clientId);
     Meteor.users.remove(clientId);
   }
