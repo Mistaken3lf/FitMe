e0737515d38fe03faa4c843023588aba090358f1
Mistaken3lf <mistaken3lf@gmail.com>
2015-08-14 11:24:37 -0500

    Changed to a collapsable accordian on client dashboard

--- client/views/ClientDashboard/clientDashboard.js
+++ client/views/ClientDashboard/clientDashboard.js
@@ -1,4 +1,7 @@
 //Activate dashboard tabs
 Template.clientDashboard.onRendered(function () {
   $('ul.tabs').tabs();
+  $('.collapsible').collapsible({
+     accordion : true // A setting that changes the collapsible behavior to expandable instead of the default accordion style
+   });
 });
