794394ba16c4ff1ea0d7a5ea1383edbbf873b17f
madams92 <mma873@lionmail.lindenwood.edu>
2015-08-19 00:36:39 -0500

    Client Cardio Page

---
+++ server/publications/ClientPublications/clientPublication.js
@@ -0,0 +1,27 @@
+Meteor.publish('currentClients', function () {
+  if(this.userId && Roles.userIsInRole(this.userId, "trainer")) {
+    return Meteor.users.find({roles: 'client', "userProfile.createdBy": this.userId}, {
+      fields: {
+        username: 1,
+        "emails.address": 1,
+        "userProfile.firstName": 1,
+        "userProfile.lastName": 1,
+        "userProfile.birthday": 1,
+        "userProfile.address": 1,
+        "userProfile.city": 1,
+        "userProfile.state": 1,
+        "userProfile.zip": 1,
+        "userProfile.homePhone": 1,
+        "userProfile.cellPhone": 1,
+        "userProfile.workPhone": 1,
+        "userProfile.emergencyContact": 1,
+        "userProfile.bio": 1,
+        "userProfile.fitnessGoals": 1,
+      }});
+    }
+
+    else {
+      throw new Meteor.Error("not-authorized");
+      return this.ready();
+    }
+});
