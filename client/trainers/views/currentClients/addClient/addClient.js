f721ff00cfb4b90fa972fa663e0e9fa345f21866
Mistaken3lf <mistaken3lf@gmail.com>
2015-08-20 17:58:26 -0500

    Added blue profile headings and fixed datepicker years

--- client/trainers/views/currentClients/addClient/addClient.js
+++ client/trainers/views/currentClients/addClient/addClient.js
@@ -1,7 +1,9 @@
 Template.addClient.onRendered(function () {
   $('.datepicker').pickadate({
     selectMonths: true, // Creates a dropdown to control month
-    selectYears: 15 // Creates a dropdown of 15 years to control year
+    selectYears: 225, // Creates a dropdown of 25 years to control year
+    autoclose: true,
+    format: "yyyy-mm-dd",
   });
 });
 
