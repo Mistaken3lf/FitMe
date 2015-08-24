095920378da695b21b02d6bd3cab9ebb969ab799
Mistaken3lf <mistaken3lf@gmail.com>
2015-08-24 15:51:51 -0500

    Fixed workout publication

--- server/trainers/publications/clientDashboardPublications/clientWorkoutPublication/clientWorkoutPublication.js
+++ server/trainers/publications/clientDashboardPublications/clientWorkoutPublication/clientWorkoutPublication.js
@@ -1,6 +1,11 @@
 Meteor.publish('currentClientsWorkout', function (currentClientsId) {
   if(this.userId && Roles.userIsInRole(this.userId, "trainer")) {
-    return ClientWorkout.find({whosWorkout: currentClientsId});
+    return ClientWorkout.find({whosWorkout: currentClientsId}, {
+      fields: {
+        whosWorkout: 0,
+        createdBy: 0,
+      }
+    });
   } else {
       throw new Meteor.Error("not-authorized");
       return this.ready();
