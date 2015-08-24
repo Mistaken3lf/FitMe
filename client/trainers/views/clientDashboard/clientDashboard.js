f218d90b165b12abbd273f599c7127620f46f2f5
Mistaken3lf <mistaken3lf@gmail.com>
2015-08-24 11:41:23 -0500

    Switched client dashboard to tabs view

--- client/trainers/views/clientDashboard/clientDashboard.js
+++ client/trainers/views/clientDashboard/clientDashboard.js
@@ -1,6 +1,4 @@
 //Activate dashboard tabs
 Template.clientDashboard.onRendered(function () {
-  $('.collapsible').collapsible({
-     accordion : true // A setting that changes the collapsible behavior to expandable instead of the default accordion style
-   });
+  $('ul.tabs').tabs();
 });
