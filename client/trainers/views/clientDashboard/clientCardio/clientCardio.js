55c43d93a3918f84b3b1479199484057f19a7c2e
Mistaken3lf <mistaken3lf@gmail.com>
2015-08-30 14:30:30 -0500

    Finished cardio calculations

--- client/trainers/views/clientDashboard/clientCardio/clientCardio.js
+++ client/trainers/views/clientDashboard/clientCardio/clientCardio.js
@@ -32,16 +32,19 @@ Template.clientCardioShell.helpers({
     return currentClientsCardio;
   },
 
+  //calculate clients max heart rate
   maxHeartRate: function() {
     var clientId = FlowRouter.getParam('_id');
     var currentClientsCardio = ClientCardio.findOne({whosCardio: clientId});
 
+    //Formula for max heart rate
     var maxHeartRate = 220 - currentClientsCardio.cardioProgramAge;
 
     return maxHeartRate;
 
   },
 
+  //Week 1-4 starting heart rate
   startBeatsPerMinuteW14: function() {
     var clientId = FlowRouter.getParam('_id');
     var currentClientsCardio = ClientCardio.findOne({whosCardio: clientId});
@@ -56,6 +59,7 @@ Template.clientCardioShell.helpers({
 
   },
 
+  //Week 1-4 ending heart rate
   endBeatsPerMinuteW14: function() {
     var clientId = FlowRouter.getParam('_id');
     var currentClientsCardio = ClientCardio.findOne({whosCardio: clientId});
@@ -67,6 +71,62 @@ Template.clientCardioShell.helpers({
     var endBeatsPerMinuteW14 = endBeatsPerMinutePart2 + currentClientsCardio.cardioW14EndRestingHeartRate;
 
     return endBeatsPerMinuteW14;
+
+  },
+
+  startBeatsPerMinuteW58: function() {
+    var clientId = FlowRouter.getParam('_id');
+    var currentClientsCardio = ClientCardio.findOne({whosCardio: clientId});
+
+    var startIntensityW58 = currentClientsCardio.cardioW58StartPercentIntensity / 100;
+
+    var startBeatsPerMinutePart1 = currentClientsCardio.cardioW58StartMaxHeartRate - currentClientsCardio.cardioW58StartRestingHeartRate;
+    var startBeatsPerMinutePart2 = startBeatsPerMinutePart1 * startIntensityW58;
+    var startBeatsPerMinuteW58 = startBeatsPerMinutePart2 + currentClientsCardio.cardioW58StartRestingHeartRate;
+
+    return startBeatsPerMinuteW58;
+
+  },
+
+  endBeatsPerMinuteW58: function() {
+    var clientId = FlowRouter.getParam('_id');
+    var currentClientsCardio = ClientCardio.findOne({whosCardio: clientId});
+
+    var endIntensityW58 = currentClientsCardio.cardioW58EndPercentIntensity / 100;
+
+    var endBeatsPerMinutePart1 = currentClientsCardio.cardioW58EndMaxHeartRate - currentClientsCardio.cardioW58EndRestingHeartRate;
+    var endBeatsPerMinutePart2 = endBeatsPerMinutePart1 * endIntensityW58;
+    var endBeatsPerMinuteW58 = endBeatsPerMinutePart2 + currentClientsCardio.cardioW58EndRestingHeartRate;
+
+    return endBeatsPerMinuteW58;
+
+  },
+
+  startBeatsPerMinuteW912: function() {
+    var clientId = FlowRouter.getParam('_id');
+    var currentClientsCardio = ClientCardio.findOne({whosCardio: clientId});
+
+    var startIntensityW912 = currentClientsCardio.cardioW912StartPercentIntensity / 100;
+
+    var startBeatsPerMinutePart1 = currentClientsCardio.cardioW912StartMaxHeartRate - currentClientsCardio.cardioW912StartRestingHeartRate;
+    var startBeatsPerMinutePart2 = startBeatsPerMinutePart1 * startIntensityW912;
+    var startBeatsPerMinuteW912 = startBeatsPerMinutePart2 + currentClientsCardio.cardioW912StartRestingHeartRate;
+
+    return startBeatsPerMinuteW912;
+
+  },
+
+  endBeatsPerMinuteW912: function() {
+    var clientId = FlowRouter.getParam('_id');
+    var currentClientsCardio = ClientCardio.findOne({whosCardio: clientId});
+
+    var endIntensityW912 = currentClientsCardio.cardioW912EndPercentIntensity / 100;
+
+    var endBeatsPerMinutePart1 = currentClientsCardio.cardioW912EndMaxHeartRate - currentClientsCardio.cardioW912EndRestingHeartRate;
+    var endBeatsPerMinutePart2 = endBeatsPerMinutePart1 * endIntensityW912;
+    var endBeatsPerMinuteW912 = endBeatsPerMinutePart2 + currentClientsCardio.cardioW912EndRestingHeartRate;
+
+    return endBeatsPerMinuteW912;
 
   }
 });
