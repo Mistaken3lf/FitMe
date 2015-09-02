0a03b4b44d0ffc1af86131037b473d019bae7151
Mistaken3lf <mistaken3lf@gmail.com>
2015-09-02 14:50:50 -0500

    Added to calendar

--- client/trainers/views/trainerSchedule/trainerSchedule.js
+++ client/trainers/views/trainerSchedule/trainerSchedule.js
@@ -1,39 +1,35 @@
 //Run when the workout template is created
-Template.trainerSchedule.onCreated(function () {
-  var self = this;
-
-  //Subscribe to the current clients workout based on url param
-  self.autorun(function () {
-    self.subscribe("trainerSchedule");
-  });
+Template.trainerSchedule.onCreated(function() {
+    var self = this;
+    //Subscribe to the current clients workout based on url param
+    self.autorun(function() {
+        self.subscribe("trainerSchedule");
+    });
 });
 
 Template.trainerSchedule.helpers({
-  events: function() {
-    var fc = $('.fc');
-
-    return function (start, end, tz, callback) {
-      Meteor.subscribe("trainerSchedule", function() {
-        fc.fullCalendar('refetchEvents');
-      });
-
-      var events = ClientWorkout.find().map(function (it) {
-        return {
-          title: "Workout Day",
-          start: it.workoutDate,
-          allDay: true,
+    events: function() {
+        var fc = $('.fc');
+        return function(start, end, tz, callback) {
+            Meteor.subscribe("trainerSchedule", function() {
+                fc.fullCalendar('refetchEvents');
+            });
+            var events = ClientWorkout.find().map(function(it) {
+                return {
+                    title: "Workout Day",
+                    start: it.workoutDate,
+                    allDay: true,
+                };
+            });
+            callback(events);
         };
-      });
-      callback(events);
-    };
-  }
+    }
 });
 
 Template.trainerSchedule.onRendered(function() {
-  var fc = this.$('.fc');
-
-  this.autorun(function () {
-    ClientWorkout.find();
-    fc.fullCalendar('refetchEvents');
-  })
+    var fc = this.$('.fc');
+    this.autorun(function() {
+        ClientWorkout.find();
+        fc.fullCalendar('refetchEvents');
+    })
 });
