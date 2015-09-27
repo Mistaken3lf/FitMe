75739c62ae2405b65f85b4a3f520999412d3ebc5
Mistaken3lf <mistaken3lf@gmail.com>
2015-09-27 15:51:24 -0500

    Fixed navigation css

--- client/allUsers/templates/navigation/navigation.js
+++ client/allUsers/templates/navigation/navigation.js
@@ -13,6 +13,7 @@ Template.navigation.events({
   }
 });
 
+//Collapse admin nav on mobile
 Template.adminNav.onRendered(function () {
   //Make mobile sidebar on left side collapsible
   $('.button-collapse').sideNav({
@@ -21,6 +22,7 @@ Template.adminNav.onRendered(function () {
   });
 });
 
+//Collapse trainer nav on mobile
 Template.trainerNav.onRendered(function () {
   //Make mobile sidebar on left side collapsible
   $('.button-collapse').sideNav({
@@ -29,7 +31,17 @@ Template.trainerNav.onRendered(function () {
   });
 });
 
+//Collapse client nav on mobile
 Template.clientNav.onRendered(function () {
+  //Make mobile sidebar on left side collapsible
+  $('.button-collapse').sideNav({
+    edge: 'left',
+    closeOnClick: true
+  });
+});
+
+//Collapse logged out nav on mobile
+Template.loggedOutNav.onRendered(function () {
   //Make mobile sidebar on left side collapsible
   $('.button-collapse').sideNav({
     edge: 'left',
