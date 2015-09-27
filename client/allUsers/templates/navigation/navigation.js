f25f70846988b1980c7a89f1feffaa892a107ec2
Mistaken3lf <mistaken3lf@gmail.com>
2015-09-27 15:59:05 -0500

    Switched back to same color scheme for admin

--- client/allUsers/templates/navigation/navigation.js
+++ client/allUsers/templates/navigation/navigation.js
@@ -13,35 +13,7 @@ Template.navigation.events({
   }
 });
 
-//Collapse admin nav on mobile
-Template.adminNav.onRendered(function () {
-  //Make mobile sidebar on left side collapsible
-  $('.button-collapse').sideNav({
-    edge: 'left',
-    closeOnClick: true
-  });
-});
-
-//Collapse trainer nav on mobile
-Template.trainerNav.onRendered(function () {
-  //Make mobile sidebar on left side collapsible
-  $('.button-collapse').sideNav({
-    edge: 'left',
-    closeOnClick: true
-  });
-});
-
-//Collapse client nav on mobile
-Template.clientNav.onRendered(function () {
-  //Make mobile sidebar on left side collapsible
-  $('.button-collapse').sideNav({
-    edge: 'left',
-    closeOnClick: true
-  });
-});
-
-//Collapse logged out nav on mobile
-Template.loggedOutNav.onRendered(function () {
+Template.navigation.onRendered(function () {
   //Make mobile sidebar on left side collapsible
   $('.button-collapse').sideNav({
     edge: 'left',
