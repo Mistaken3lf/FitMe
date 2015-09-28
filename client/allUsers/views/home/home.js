2adf0ef0b40f051dfd3be71f85adbfc995fa0bd0
Mistaken3lf <mistaken3lf@gmail.com>
2015-09-28 18:37:36 -0500

    Home page files cleanup

--- client/allUsers/views/home/home.js
+++ client/allUsers/views/home/home.js
@@ -1,25 +1,5 @@
 Template.home.onRendered(function () {
-  $('.datepicker').pickadate({
-    selectMonths: true,
-    selectYears: 225,
-    autoclose: true,
-    format: "yyyy-mm-dd",
+  $(document).ready(function () {
+    $('input#input_text, textarea#textarea1').characterCounter();
   });
-
-$(document).ready(function(){
-      $('.parallax').parallax();
-    });
-
-
-$(document).ready(function(){
-      $('.slider').slider({full_width: true});
-    });
-
-
-    $(document).ready(function() {
-        $('input#input_text, textarea#textarea1').characterCounter();
-      });
-
-
-
-    });
+});
