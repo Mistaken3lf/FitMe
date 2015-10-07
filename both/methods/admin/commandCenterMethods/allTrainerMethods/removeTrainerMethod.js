01a9f7ae44e020f542f0be16c1738fd518d58f4a
Mistaken3lf <mistaken3lf@gmail.com>
2015-10-07 16:26:14 -0500

    Fixed deletetion of users

--- both/methods/admin/commandCenterMethods/allTrainerMethods/removeTrainerMethod.js
+++ both/methods/admin/commandCenterMethods/allTrainerMethods/removeTrainerMethod.js
@@ -5,6 +5,18 @@ Meteor.methods({
     if (!Meteor.userId() && Roles.userIsInRole(this.userId, "admin")) {
       throw new Meteor.Error("not-authorized");
     }
+
+    //Remove cardio associated with the trainer being deleted
+    ClientCardio.remove({createdBy: trainerId});
+
+    //Remove client stats associated with the trainer being deleted
+    ClientStats.remove({createdBy: trainerId});
+
+    //Remove client workouts associated with the trainer being deleted
+    ClientWorkout.remove({createdBy: trainerId});
+
+    //Remove clients associated with the current trainer you are deleting
+    Meteor.users.remove({"userProfile.createdBy": trainerId});
     
     //Remove trainer clicked on
     Meteor.users.remove(trainerId);
--- both/methods/trainers/deleteClientMethod/deleteClientMethod.js
+++ both/methods/trainers/deleteClientMethod/deleteClientMethod.js
@@ -9,7 +9,17 @@ Meteor.methods({
       throw new Meteor.Error("not-authorized");
     }
 
+    //Subtract one from the trainers client limit since they are removing a client
     Meteor.users.update({ _id: this.userId }, { $inc: { clientLimit: -1 } });
+
+    //Remove cardio of the client being deleted
+    ClientCardio.remove({whosCardio: clientId});
+
+    //Remove stats of the client being deleted
+    ClientStats.remove({whosStats: clientId});
+
+    //Remove workout of client being deleted
+    ClientWorkout.remove({whosWorkout: clientId});
 
     //Delete the client clicked on
     Meteor.users.remove(clientId);
