ffb51331fbe10588e78b4d2f81232211c3689de9
Tyler <mistaken3lf@gmail.com>
2015-10-13 17:56:17 +0000

    Added stats measurement changes

--- client/views/allUsers/home/home.js
+++ client/views/allUsers/home/home.js
@@ -6,14 +6,12 @@ Template.home.onRendered(function () {
 Template.home.events({
   'submit form': function (event) {
     event.preventDefault();
-
     //Get who send the message and the actual message
     var firstName = $('[name=firstName]').val();
     var lastName = $('[name=lastName]').val();
     var phoneNumber = $('[name=phoneNumber]').val();
     var email = $('[name=email]').val();
     var message = $('[name=message]').val();
-
     //Call the send feedback method to send us information.
     Meteor.call("contactUs", firstName, lastName, phoneNumber, email, message, function (error) {
       if (error) {
--- client/views/trainers/clientDashboard/clientStats/clientStats.js
+++ client/views/trainers/clientDashboard/clientStats/clientStats.js
@@ -1,16 +1,14 @@
-Template.clientStats.onCreated(function() {
+Template.clientStats.onCreated(function () {
   var self = this;
 
   //Subscribe to the current clients stats based on url param
-  self.autorun(function() {
+  self.autorun(function () {
     var clientId = FlowRouter.getParam('_id');
     self.subscribe("currentClientsStats", clientId);
   });
 });
 
-
-
-Template.clientStatsShell.onRendered(function() {
+Template.clientStatsShell.onRendered(function () {
   //Pop up date picker when a date field is selected
   $('.datepicker').pickadate({
     selectMonths: true,
@@ -20,11 +18,9 @@ Template.clientStatsShell.onRendered(function() {
   });
 });
 
-
-
 Template.clientStatsShell.helpers({
   //Get the current clients stats values
-  currentClientsStats: function() {
+  currentClientsStats: function () {
     var clientId = FlowRouter.getParam('_id');
     var currentClientsStats = ClientStats.findOne({
       whosStats: clientId
@@ -36,7 +32,7 @@ Template.clientStatsShell.helpers({
 
   //Get the currrent clients profile info to display their first and
   //last name
-  currentClient: function() {
+  currentClient: function () {
     var clientId = FlowRouter.getParam('_id');
     var currentClient = Meteor.users.findOne({
       'userProfile.whosProfile': clientId
@@ -45,7 +41,7 @@ Template.clientStatsShell.helpers({
   },
 
   //Calculate clients body weight change from retest 1 and 3
-  bodyWeightChange: function() {
+  bodyWeightChange: function () {
     var clientId = FlowRouter.getParam('_id');
     var currentClient = ClientStats.findOne({
       whosStats: clientId
@@ -57,7 +53,7 @@ Template.clientStatsShell.helpers({
   },
 
   //Calculate the body fat percent change from retest 1 and 3
-  bodyFatPercentChange: function() {
+  bodyFatPercentChange: function () {
     var clientId = FlowRouter.getParam('_id');
     var currentClient = ClientStats.findOne({
       whosStats: clientId
@@ -69,7 +65,7 @@ Template.clientStatsShell.helpers({
   },
 
   //Calculate the change in clients pounds of body fat from retest 1 and 3
-  changeInPounds: function() {
+  changeInPounds: function () {
     var clientId = FlowRouter.getParam('_id');
     var currentClient = ClientStats.findOne({
       whosStats: clientId
@@ -81,7 +77,7 @@ Template.clientStatsShell.helpers({
   },
 
   //Calculate clients change in lean mass from retest 1 and 3
-  leanMass: function() {
+  leanMass: function () {
     var clientId = FlowRouter.getParam('_id');
     var currentClient = ClientStats.findOne({
       whosStats: clientId
@@ -93,7 +89,7 @@ Template.clientStatsShell.helpers({
   },
 
   //Calculate clients body fat lost from retest 1 and 3
-  fatLost: function() {
+  fatLost: function () {
     var clientId = FlowRouter.getParam('_id');
     var currentClient = ClientStats.findOne({
       whosStats: clientId
@@ -105,7 +101,7 @@ Template.clientStatsShell.helpers({
   },
 
   //Calculate clients muscle gain from retest 1 and 3
-  muscleGained: function() {
+  muscleGained: function () {
     var clientId = FlowRouter.getParam('_id');
     var currentClient = ClientStats.findOne({
       whosStats: clientId
@@ -115,4 +111,103 @@ Template.clientStatsShell.helpers({
 
     return muscleGained;
   },
+
+  neckMeasureChange: function () {
+    var clientId = FlowRouter.getParam('_id');
+    var currentClient = ClientStats.findOne({
+      whosStats: clientId
+    });
+
+    var neckMeasureChange = currentClient.measureRT3Neck - currentClient.measureRT1Neck;
+
+    return neckMeasureChange;
+  },
+
+  chestMeasureChange: function () {
+    var clientId = FlowRouter.getParam('_id');
+    var currentClient = ClientStats.findOne({
+      whosStats: clientId
+    });
+
+    var chestMeasureChange = currentClient.measureRT3Chest - currentClient.measureRT1Chest;
+
+    return chestMeasureChange;
+  },
+
+  shoulderMeasureChange: function () {
+    var clientId = FlowRouter.getParam('_id');
+    var currentClient = ClientStats.findOne({
+      whosStats: clientId
+    });
+
+    var shoulderMeasureChange = currentClient.measureRT3Shoulders - currentClient.measureRT1Shoulders;
+
+    return shoulderMeasureChange;
+  },
+
+  waistMeasureChange: function () {
+    var clientId = FlowRouter.getParam('_id');
+    var currentClient = ClientStats.findOne({
+      whosStats: clientId
+    });
+
+    var waistMeasureChange = currentClient.measureRT3Waist - currentClient.measureRT1Waist;
+
+    return waistMeasureChange;
+  },
+
+  hipsMeasureChange: function () {
+    var clientId = FlowRouter.getParam('_id');
+    var currentClient = ClientStats.findOne({
+      whosStats: clientId
+    });
+
+    var hipsMeasureChange = currentClient.measureRT3Hips - currentClient.measureRT1Hips;
+
+    return hipsMeasureChange;
+  },
+
+  thighMeasureChange: function () {
+    var clientId = FlowRouter.getParam('_id');
+    var currentClient = ClientStats.findOne({
+      whosStats: clientId
+    });
+
+    var thighMeasureChange = currentClient.measureRT3Thigh - currentClient.measureRT1Thigh;
+
+    return thighMeasureChange;
+  },
+
+  calfMeasureChange: function () {
+    var clientId = FlowRouter.getParam('_id');
+    var currentClient = ClientStats.findOne({
+      whosStats: clientId
+    });
+
+    var calfMeasureChange = currentClient.measureRT3Calf - currentClient.measureRT1Calf;
+
+    return calfMeasureChange;
+  },
+
+  bicepMeasureChange: function () {
+    var clientId = FlowRouter.getParam('_id');
+    var currentClient = ClientStats.findOne({
+      whosStats: clientId
+    });
+
+    var bicepMeasureChange = currentClient.measureRT3Bicep - currentClient.measureRT1Bicep;
+
+    return bicepMeasureChange;
+  },
+
+  forearmMeasureChange: function () {
+    var clientId = FlowRouter.getParam('_id');
+    var currentClient = ClientStats.findOne({
+      whosStats: clientId
+    });
+
+    var forearmMeasureChange = currentClient.measureRT3Forearm - currentClient.measureRT1Forearm;
+
+    return forearmMeasureChange;
+  }
 });
