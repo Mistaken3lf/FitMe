4a81d8217fa8eb128c1b476847ebf18ce1c629cd
Mistaken3lf <mistaken3lf@gmail.com>
2015-10-27 16:34:22 +0000

    Created my own tab system

--- client/views/trainers/clientDashboard/clientDashboard.js
+++ client/views/trainers/clientDashboard/clientDashboard.js
@@ -9,5 +9,17 @@ Template.clientDashboard.helpers({
   //Check if the user is currently logging in
   isLoggingIn: function () {
     return Meteor.loggingIn();
+  },
+  
+  activeTemplate: function () {
+    return Session.get("clickedButton");
+  }
+});
+
+Template.clientDashboard.events({
+  "click .clickedButton": function (event) {
+    var clickedButton = event.target.id;
+    Session.set("clickedButton", clickedButton);
+    console.log(clickedButton);
   }
 });
