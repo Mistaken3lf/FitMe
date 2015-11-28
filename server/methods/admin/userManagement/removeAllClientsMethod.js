38cac7c3c4a7720c4d7cb48559b2b3022dd3452f
Tyler <mistaken3lf@gmail.com>
2015-11-28 16:07:49 +0000

    When resetting an account, cardio, workouts etc. are removed as well as all clients

--- server/methods/admin/userManagement/removeAllClientsMethod.js
+++ server/methods/admin/userManagement/removeAllClientsMethod.js
@@ -5,6 +5,21 @@ Meteor.methods({
       Meteor.users.remove({
         createdBy: trainerId
       });
+      
+      //Remove cardio of the client being deleted
+      ClientCardio.remove({
+        createdBy: trainerId
+      });
+
+      //Remove stats of the client being deleted
+      ClientStats.remove({
+        createdBy: trainerId
+      });
+
+      //Remove workout of client being deleted
+      ClientWorkout.remove({
+        createdBy: trainerId
+      });
 
     } else {
       throw new Meteor.Error("not-authorized");
