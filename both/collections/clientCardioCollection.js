a56a78a61be96555f441833fd18cc7c108e761ff
Tyler <mistaken3lf@gmail.com>
2015-12-15 20:46:27 +0000

    Allow decimals to cardio and stats

--- both/collections/clientCardioCollection.js
+++ both/collections/clientCardioCollection.js
@@ -20,11 +20,13 @@ ClientCardioSchema.cardio = new SimpleSchema({
   cardioProgramRestingHeartRate: {
     type: Number,
     optional: true,
+    decimal: true
   },
 
   cardioProgramMaxHeartRate: {
     type: Number,
     optional: true,
+    decimal: true
   },
   //End Of Cardio Program Basics
 
@@ -37,21 +39,25 @@ ClientCardioSchema.cardio = new SimpleSchema({
   cardioW14StartMaxHeartRate: {
     type: Number,
     optional: true,
+    decimal: true
   },
 
   cardioW14StartRestingHeartRate: {
     type: Number,
     optional: true,
+    decimal: true
   },
 
   cardioW14StartPercentIntensity: {
     type: Number,
     optional: true,
+    decimal: true
   },
 
   cardioW14StartBPM: {
     type: Number,
     optional: true,
+    decimal: true
   },
 
   cardioW14EndDate: {
@@ -62,41 +68,49 @@ ClientCardioSchema.cardio = new SimpleSchema({
   cardioW14EndMaxHeartRate: {
     type: Number,
     optional: true,
+    decimal: true
   },
 
   cardioW14EndRestingHeartRate: {
     type: Number,
     optional: true,
+    decimal: true
   },
 
   cardioW14EndPercentIntensity: {
     type: Number,
     optional: true,
+    decimal: true
   },
 
   cardioW14EndBPM: {
     type: Number,
     optional: true,
+    decimal: true
   },
 
   cardioW14Beginner: {
     type: String,
     optional: true,
+    decimal: true
   },
 
   cardioW14Intermediate: {
     type: String,
     optional: true,
+    decimal: true
   },
 
   cardioW14Advanced: {
     type: String,
     optional: true,
+    decimal: true
   },
 
   cardioW14Type: {
     type: String,
     optional: true,
+    decimal: true
   },
   //End Of Cardio Program Weeks 1 - 4
 
@@ -109,21 +123,25 @@ ClientCardioSchema.cardio = new SimpleSchema({
   cardioW58StartMaxHeartRate: {
     type: Number,
     optional: true,
+    decimal: true
   },
 
   cardioW58StartRestingHeartRate: {
     type: Number,
     optional: true,
+    decimal: true
   },
 
   cardioW58StartPercentIntensity: {
     type: Number,
     optional: true,
+    decimal: true
   },
 
   cardioW58StartBPM: {
     type: Number,
     optional: true,
+    decimal: true
   },
 
   cardioW58EndDate: {
@@ -134,16 +152,19 @@ ClientCardioSchema.cardio = new SimpleSchema({
   cardioW58EndMaxHeartRate: {
     type: Number,
     optional: true,
+    decimal: true
   },
 
   cardioW58EndRestingHeartRate: {
     type: Number,
     optional: true,
+    decimal: true
   },
 
   cardioW58EndPercentIntensity: {
     type: Number,
     optional: true,
+    decimal: true
   },
 
   cardioW58EndBPM: {
@@ -154,21 +175,25 @@ ClientCardioSchema.cardio = new SimpleSchema({
   cardioW58Beginner: {
     type: String,
     optional: true,
+    decimal: true
   },
 
   cardioW58Intermediate: {
     type: String,
     optional: true,
+    decimal: true
   },
 
   cardioW58Advanced: {
     type: String,
     optional: true,
+    decimal: true
   },
 
   cardioW58Type: {
     type: String,
     optional: true,
+    decimal: true
   },
   //End Of Cardio Program Weeks 5 - 8
 
@@ -181,21 +206,25 @@ ClientCardioSchema.cardio = new SimpleSchema({
   cardioW912StartMaxHeartRate: {
     type: Number,
     optional: true,
+    decimal: true
   },
 
   cardioW912StartRestingHeartRate: {
     type: Number,
     optional: true,
+    decimal: true
   },
 
   cardioW912StartPercentIntensity: {
     type: Number,
     optional: true,
+    decimal: true
   },
 
   cardioW912StartBPM: {
     type: Number,
     optional: true,
+    decimal: true
   },
 
   cardioW912EndDate: {
@@ -206,41 +235,49 @@ ClientCardioSchema.cardio = new SimpleSchema({
   cardioW912EndMaxHeartRate: {
     type: Number,
     optional: true,
+    decimal: true
   },
 
   cardioW912EndRestingHeartRate: {
     type: Number,
     optional: true,
+    decimal: true
   },
 
   cardioW912EndPercentIntensity: {
     type: Number,
     optional: true,
+    decimal: true
   },
 
   cardioW912EndBPM: {
     type: Number,
     optional: true,
+    decimal: true
   },
 
   cardioW912Beginner: {
     type: String,
     optional: true,
+    decimal: true
   },
 
   cardioW912Intermediate: {
     type: String,
     optional: true,
+    decimal: true
   },
 
   cardioW912Advanced: {
     type: String,
     optional: true,
+    decimal: true
   },
 
   cardioW912Type: {
     type: String,
     optional: true,
+    decimal: true
   },
   //End Of Cardio Program Weeks 9 - 12
 });
--- both/collections/clientStatsCollection.js
+++ both/collections/clientStatsCollection.js
@@ -20,21 +20,25 @@ ClientStatsSchema.stats = new SimpleSchema({
   statsITBodyWeight: {
     type: Number,
     optional: true,
+    decimal: true
   },
 
   statsITBodyFatPercent: {
     type: Number,
     optional: true,
+    decimal: true
   },
 
   statsITBodyFatLbs: {
     type: Number,
     optional: true,
+    decimal: true
   },
 
   statsITLeanMass: {
     type: Number,
     optional: true,
+    decimal: true
   },
   //End Of Body Statistics Initial Test
 
@@ -42,73 +46,86 @@ ClientStatsSchema.stats = new SimpleSchema({
   statsRT1Date: {
     type: String,
     optional: true,
+    decimal: true
   },
 
   statsRT1BodyWeight: {
     type: Number,
     optional: true,
+    decimal: true
   },
 
   statsRT1BodyFatPercent: {
     type: Number,
     optional: true,
+    decimal: true
   },
 
   statsRT1BodyFatLbs: {
     type: Number,
     optional: true,
+    decimal: true
   },
 
   statsRT1LeanMass: {
     type: Number,
     optional: true,
+    decimal: true
   },
 
   statsRT1BodyFatLost: {
     type: Number,
     optional: true,
+    decimal: true
   },
 
   statsRT1LeanMuscleGain: {
     type: Number,
     optional: true,
+    decimal: true
   },
   //End Of Body Statistics Retest 1
 
   //Body Statistics Retest 2
   statsRT2Date: {
     type: String,
-    optional: true,
+    optional: true
   },
 
   statsRT2BodyWeight: {
     type: Number,
     optional: true,
+    decimal: true
   },
 
   statsRT2BodyFatPercent: {
     type: Number,
     optional: true,
+    decimal: true
   },
 
   statsRT2BodyFatLbs: {
     type: Number,
     optional: true,
+    decimal: true
   },
 
   statsRT2LeanMass: {
     type: Number,
     optional: true,
+    decimal: true
   },
 
   statsRT2BodyFatLost: {
     type: Number,
     optional: true,
+    decimal: true
   },
 
   statsRT2LeanMuscleGain: {
     type: Number,
     optional: true,
+    decimal: true
   },
   //End Of Body Statistics Retest 2
 
@@ -116,36 +133,43 @@ ClientStatsSchema.stats = new SimpleSchema({
   statsRT3Date: {
     type: String,
     optional: true,
+    decimal: true
   },
 
   statsRT3BodyWeight: {
     type: Number,
     optional: true,
+    decimal: true
   },
 
   statsRT3BodyFatPercent: {
     type: Number,
     optional: true,
+    decimal: true
   },
 
   statsRT3BodyFatLbs: {
     type: Number,
     optional: true,
+    decimal: true
   },
 
   statsRT3LeanMass: {
     type: Number,
     optional: true,
+    decimal: true
   },
 
   statsRT3BodyFatLost: {
     type: Number,
     optional: true,
+    decimal: true
   },
 
   statsRT3LeanMuscleGain: {
     type: Number,
     optional: true,
+    decimal: true
   },
   //End Of Body Statistics Retest 3
 
@@ -158,31 +182,37 @@ ClientStatsSchema.stats = new SimpleSchema({
   statsChangeBodyWeight: {
     type: Number,
     optional: true,
+    decimal: true
   },
 
   statsChangeBodyFatPercent: {
     type: Number,
     optional: true,
+    decimal: true
   },
 
   statsChangeBodyFatLbs: {
     type: Number,
     optional: true,
+    decimal: true
   },
 
   statsChangeLeanMass: {
     type: Number,
     optional: true,
+    decimal: true
   },
 
   statsChangeBodyFatLost: {
     type: Number,
     optional: true,
+    decimal: true
   },
 
   statsChangeLeanMuscleGain: {
     type: Number,
     optional: true,
+    decimal: true
   },
   //End Of Body Statistics Change
 
@@ -195,46 +225,55 @@ ClientStatsSchema.stats = new SimpleSchema({
   measureITNeck: {
     type: Number,
     optional: true,
+    decimal: true
   },
 
   measureITChest: {
     type: Number,
     optional: true,
+    decimal: true
   },
 
   measureITShoulders: {
     type: Number,
     optional: true,
+    decimal: true
   },
 
   measureITWaist: {
     type: Number,
     optional: true,
+    decimal: true
   },
 
   measureITHips: {
     type: Number,
     optional: true,
+    decimal: true
   },
 
   measureITThigh: {
     type: Number,
     optional: true,
+    decimal: true
   },
 
   measureITCalf: {
     type: Number,
     optional: true,
+    decimal: true
   },
 
   measureITBicep: {
     type: Number,
     optional: true,
+    decimal: true
   },
 
   measureITForearm: {
     type: Number,
     optional: true,
+    decimal: true
   },
   //End Of Initial Test Measurements
 
@@ -242,51 +281,61 @@ ClientStatsSchema.stats = new SimpleSchema({
   measureRT1Date: {
     type: String,
     optional: true,
+    decimal: true
   },
 
   measureRT1Neck: {
     type: Number,
     optional: true,
+    decimal: true
   },
 
   measureRT1Chest: {
     type: Number,
     optional: true,
+    decimal: true
   },
 
   measureRT1Shoulders: {
     type: Number,
     optional: true,
+    decimal: true
   },
 
   measureRT1Waist: {
     type: Number,
     optional: true,
+    decimal: true
   },
 
   measureRT1Hips: {
     type: Number,
     optional: true,
+    decimal: true
   },
 
   measureRT1Thigh: {
     type: Number,
     optional: true,
+    decimal: true
   },
 
   measureRT1Calf: {
     type: Number,
     optional: true,
+    decimal: true
   },
 
   measureRT1Bicep: {
     type: Number,
     optional: true,
+    decimal: true
   },
 
   measureRT1Forearm: {
     type: Number,
     optional: true,
+    decimal: true
   },
   //End Of Retest 1 Measurements
 
@@ -299,46 +348,55 @@ ClientStatsSchema.stats = new SimpleSchema({
   measureRT2Neck: {
     type: Number,
     optional: true,
+    decimal: true
   },
 
   measureRT2Chest: {
     type: Number,
     optional: true,
+    decimal: true
   },
 
   measureRT2Shoulders: {
     type: Number,
     optional: true,
+    decimal: true
   },
 
   measureRT2Waist: {
     type: Number,
     optional: true,
+    decimal: true
   },
 
   measureRT2Hips: {
     type: Number,
     optional: true,
+    decimal: true
   },
 
   measureRT2Thigh: {
     type: Number,
     optional: true,
+    decimal: true
   },
 
   measureRT2Calf: {
     type: Number,
     optional: true,
+    decimal: true
   },
 
   measureRT2Bicep: {
     type: Number,
     optional: true,
+    decimal: true
   },
 
   measureRT2Forearm: {
     type: Number,
     optional: true,
+    decimal: true
   },
   //End Of Retest 2 Measurements
 
@@ -346,51 +404,61 @@ ClientStatsSchema.stats = new SimpleSchema({
   measureRT3Date: {
     type: String,
     optional: true,
+    decimal: true
   },
 
   measureRT3Neck: {
     type: Number,
     optional: true,
+    decimal: true
   },
 
   measureRT3Chest: {
     type: Number,
     optional: true,
+    decimal: true
   },
 
   measureRT3Shoulders: {
     type: Number,
     optional: true,
+    decimal: true
   },
 
   measureRT3Waist: {
     type: Number,
     optional: true,
+    decimal: true
   },
 
   measureRT3Hips: {
     type: Number,
     optional: true,
+    decimal: true
   },
 
   measureRT3Thigh: {
     type: Number,
     optional: true,
+    decimal: true
   },
 
   measureRT3Calf: {
     type: Number,
     optional: true,
+    decimal: true
   },
 
   measureRT3Bicep: {
     type: Number,
     optional: true,
+    decimal: true
   },
 
   measureRT3Forearm: {
     type: Number,
     optional: true,
+    decimal: true
   },
   //End Of Retest 3 Measurements
 
@@ -403,46 +471,55 @@ ClientStatsSchema.stats = new SimpleSchema({
   measureChangeNeck: {
     type: Number,
     optional: true,
+    decimal: true
   },
 
   measureChangeChest: {
     type: Number,
     optional: true,
+    decimal: true
   },
 
   measureChangeShoulders: {
     type: Number,
     optional: true,
+    decimal: true
   },
 
   measureChangeWaist: {
     type: Number,
     optional: true,
+    decimal: true
   },
 
   measureChangeHips: {
     type: Number,
     optional: true,
+    decimal: true
   },
 
   measureChangeThigh: {
     type: Number,
     optional: true,
+    decimal: true
   },
 
   measureChangeCalf: {
     type: Number,
     optional: true,
+    decimal: true
   },
 
   measureChangeBicep: {
     type: Number,
     optional: true,
+    decimal: true
   },
 
   measureChangeForearm: {
     type: Number,
     optional: true,
+    decimal: true
   },
   //End Of Retest 3 Measurements
 });
