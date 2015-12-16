f0c76c7e3cdd0cbfd3666806d135af0afbd00b52
Tyler <mistaken3lf@gmail.com>
2015-12-16 22:46:37 +0000

    Removed stripe startup

--- client/stripe.js
+++
@@ -1,9 +0,0 @@
-Meteor.startup(function () {
-  let stripeKey = Meteor.settings.public.stripe.testPublishableKey;
-  Stripe.setPublishableKey(stripeKey);
-
-  var handler = StripeCheckout.configure({
-    key: stripeKey,
-    token: function (token) {}
-  });
-});
