5295d5d755caa98a77f04c3717df5b0a4c12bbbb
Tyler <mistaken3lf@gmail.com>
2015-12-22 22:39:56 +0000

    Added learn more for free plans button

--- client/views/allUsers/home/home.js
+++ client/views/allUsers/home/home.js
@@ -6,3 +6,13 @@ Template.home.onRendered(function () {
 
   $('.parallax').parallax();
 });
+
+Template.home.events({
+  "click .learnMore": function (event) {
+    swal({
+      title: "Free Account!",
+      text: "Here's a custom image.",
+      imageUrl: "navigation/fitMeSidebarLogo.png"
+    });
+  }
+});
