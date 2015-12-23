1b93867ba81735b2cc347dfd8c4a09dd62de26f4
Tyler <mistaken3lf@gmail.com>
2015-12-23 16:38:24 +0000

    Added to the trainer guide

---
+++ client/views/trainers/trainerGuide/trainerGuide.js
@@ -0,0 +1,12 @@
+Template.trainerGuide.onRendered(() => {
+    $('.collapsible').collapsible({
+      accordion : false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
+    });
+});
+
+Template.trainerGuide.helpers({
+  //Check if the user is logging in 
+  isLoggingIn: function () {
+    return Meteor.loggingIn();
+  }
+});
