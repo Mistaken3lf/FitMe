e2a024ace69cc72d70b6e327dc4477352e85f4a9
Tyler <mistaken3lf@gmail.com>
2015-12-23 16:23:13 +0000

    Added extra purchases to the trainers account

--- client/views/trainers/myAccount/myAccount.js
+++ client/views/trainers/myAccount/myAccount.js
@@ -4,15 +4,15 @@ let planType = "";
 let dollarAmount = 0
 
 Template.myAccount.onCreated(function () {
-  var self = this;
-
   //Subscribe all the trainers current clients
-  self.autorun(function () {
-    self.subscribe("myProfile");
-    self.subscribe("currentClients");
+  this.autorun(() => {
+    this.subscribe("myProfile");
+    this.subscribe("currentClients");
   });
 
   let template = Template.instance();
+
+  template.processing = new ReactiveVar(false);
 
   if (!Meteor.loggingIn()) {
     //Find the current trainer
@@ -22,16 +22,14 @@ Template.myAccount.onCreated(function () {
 
     let email = currentTrainer.emails[0].address;
 
-    template.processing = new ReactiveVar(false);
-
     //Charge the trainer with stripe
     template.checkout = StripeCheckout.configure({
       key: Meteor.settings.public.stripe,
       locale: 'auto',
       token(token) {
         charge = {
-          amount: dollarAmount,
-          currency: 'usd',
+          amount: token.amount || dollarAmount,
+          currency: token.currenty || 'usd',
           source: token.id,
           description: planType,
           receipt_email: email
@@ -59,6 +57,21 @@ Template.myAccount.onCreated(function () {
             if (planType == "One Year") {
               Bert.alert('Thank You For Choosing FitMe', 'success');
               Meteor.call("oneYearPlanTrainer", Meteor.userId());
+            }
+            
+            if (planType == "Five Additional Clients") {
+              Bert.alert("Thank You For Choosing FitMe", "success");
+              Meteor.call("fiveAdditionalClientsTrainer", Meteor.userId());
+            }
+            
+            if (planType == "Ten Additional Clients") {
+              Bert.alert("Thank You For Choosing FitMe", "success");
+              Meteor.call("tenAdditionalClientsTrainer", Meteor.userId());
+            }
+            
+            if (planType == "Twenty Additional Clients") {
+              Bert.alert("Thank You For Choosing FitMe", "success");
+              Meteor.call("twentyAdditionalClientsTrainer", Meteor.userId());
             }
           }
         });
@@ -133,7 +146,7 @@ Template.myAccount.events({
     //Sweet alert to confirm deletion of client
     swal({
       title: "Are you sure?",
-      text: "You will be prompted for payment provided by Stripe. All payments are securely stored and handled through the Stripe website.  By clicking Yes, you will agree to these terms and will proceed to make your payment with FitMe.",      
+      text: "You will be prompted for payment provided by Stripe. All payments are securely stored and handled through the Stripe website.  By clicking Yes, you will agree to these terms and will proceed to make your payment with FitMe.",
       type: "warning",
       showCancelButton: true,
       confirmButtonColor: "#DD6B55",
@@ -164,21 +177,22 @@ Template.myAccount.events({
     //Sweet alert to confirm deletion of client
     swal({
       title: "Terms and Conditions",
-      text: "You will be prompted for payment provided by stripe all payments are handled through them securely over https",
+      text: "You will be prompted for payment provided by Stripe. All payments are securely stored and handled through the Stripe website.  By clicking Accept, you will agree to these terms and will proceed to make your payment with FitMe.",
       type: "warning",
       showCancelButton: true,
       confirmButtonColor: "#DD6B55",
-      confirmButtonText: "Yes, continue!",
+      confirmButtonText: "Accept",
       closeOnConfirm: true
     }, function (isConfirm) {
       window.onkeydown = previousWindowKeyDown;
       if (isConfirm) {
+        template.processing.set(true);
         template.checkout.open({
           email: currentTrainer.emails[0].address,
           name: 'One Month',
           description: "1 Month Of Access",
           amount: 2000,
-          bitcoin: false
+          bitcoin: true
         });
 
         planType = "One Month";
@@ -202,21 +216,22 @@ Template.myAccount.events({
     //Sweet alert to confirm deletion of client
     swal({
       title: "Terms and Conditions",
-      text: "You will be prompted for payment provided by stripe all payments are handled through them securely over https",
+      text: "You will be prompted for payment provided by Stripe. All payments are securely stored and handled through the Stripe website.  By clicking Accept, you will agree to these terms and will proceed to make your payment with FitMe.",
       type: "warning",
       showCancelButton: true,
       confirmButtonColor: "#DD6B55",
-      confirmButtonText: "Yes, continue!",
+      confirmButtonText: "Accept",
       closeOnConfirm: true
     }, function (isConfirm) {
       window.onkeydown = previousWindowKeyDown;
       if (isConfirm) {
+        teplate.processing.set(true);
         template.checkout.open({
           email: currentTrainer.emails[0].address,
           name: 'Six Month',
-          description: "Six Month's Of Access",
+          description: "Six Months Of Access",
           amount: 6000,
-          bitcoin: false
+          bitcoin: true
         });
 
         planType = "Six Month";
@@ -240,21 +255,22 @@ Template.myAccount.events({
     //Sweet alert to confirm deletion of client
     swal({
       title: "Terms and Conditions",
-      text: "You will be prompted for payment provided by stripe all payments are handled through them securely over https",
+      text: "You will be prompted for payment provided by Stripe. All payments are securely stored and handled through the Stripe website.  By clicking Accept, you will agree to these terms and will proceed to make your payment with FitMe.",
       type: "warning",
       showCancelButton: true,
       confirmButtonColor: "#DD6B55",
-      confirmButtonText: "Yes, continue!",
+      confirmButtonText: "Accept",
       closeOnConfirm: true
     }, function (isConfirm) {
       window.onkeydown = previousWindowKeyDown;
       if (isConfirm) {
+        template.processing.set(true);
         template.checkout.open({
           email: currentTrainer.emails[0].address,
           name: 'One Year',
           description: "1 Year Of Access",
           amount: 11000,
-          bitcoin: false
+          bitcoin: true
         });
 
         planType = "One Year";
@@ -281,11 +297,11 @@ Template.myAccount.events({
       //Sweet alert to confirm deletion of client
       swal({
         title: "Are You Sure?",
-        text: "You are currently enrolled in a plan and you will lose all your current clients and your client limit will be dropped to 1",
+        text: "You are currently enrolled in a plan and you will lose all your current clients and your client limit will be dropped to 1.",
         type: "warning",
         showCancelButton: true,
         confirmButtonColor: "#DD6B55",
-        confirmButtonText: "Yes, continue!",
+        confirmButtonText: "Yes",
         closeOnConfirm: true
       }, function (isConfirm) {
         window.onkeydown = previousWindowKeyDown;
@@ -319,5 +335,122 @@ Template.myAccount.events({
         }
       });
     }
-  }
+  },
+  
+  //Purchase 5 additional clients
+  'click .fiveAdditionalClients' (event, template) {
+    let currentTrainer = Meteor.users.findOne({
+      _id: Meteor.userId()
+    });
+
+    //Needed for sweet alerts
+    var previousWindowKeyDown = window.onkeydown;
+
+    //Sweet alert to confirm deletion of client
+    swal({
+      title: "Terms and Conditions",
+      text: "You will be prompted for payment provided by Stripe. All payments are securely stored and handled through the Stripe website.  By clicking Accept, you will agree to these terms and will proceed to make your payment with FitMe.",
+      type: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#DD6B55",
+      confirmButtonText: "Accept",
+      closeOnConfirm: true
+    }, function (isConfirm) {
+      window.onkeydown = previousWindowKeyDown;
+      if (isConfirm) {
+        template.processing.set(true);
+        template.checkout.open({
+          email: currentTrainer.emails[0].address,
+          name: '5 Additional Clients',
+          description: "Add five additional clients to your client limit",
+          amount: 500,
+          bitcoin: true
+        });
+
+        planType = "Five Additional Clients";
+        dollarAmount = 500;
+
+      } else {
+        swal('Cancelled', 'You Will Not Be Charged.', 'error');
+      }
+    });
+  },
+  
+    //Purchase 10 additional clients
+  'click .tenAdditionalClients' (event, template) {
+    let currentTrainer = Meteor.users.findOne({
+      _id: Meteor.userId()
+    });
+
+    //Needed for sweet alerts
+    var previousWindowKeyDown = window.onkeydown;
+
+    //Sweet alert to confirm deletion of client
+    swal({
+      title: "Terms and Conditions",
+      text: "You will be prompted for payment provided by Stripe. All payments are securely stored and handled through the Stripe website.  By clicking Accept, you will agree to these terms and will proceed to make your payment with FitMe.",
+      type: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#DD6B55",
+      confirmButtonText: "Accept",
+      closeOnConfirm: true
+    }, function (isConfirm) {
+      window.onkeydown = previousWindowKeyDown;
+      if (isConfirm) {
+        template.processing.set(true);
+        template.checkout.open({
+          email: currentTrainer.emails[0].address,
+          name: '10 Additional Clients',
+          description: "Add ten additional clients to your client limit",
+          amount: 900,
+          bitcoin: true
+        });
+
+        planType = "Ten Additional Clients";
+        dollarAmount = 900;
+
+      } else {
+        swal('Cancelled', 'You Will Not Be Charged.', 'error');
+      }
+    });
+  },
+  
+    //Purchase 20 additional clients
+  'click .twentyAdditionalClients' (event, template) {
+    let currentTrainer = Meteor.users.findOne({
+      _id: Meteor.userId()
+    });
+
+    //Needed for sweet alerts
+    var previousWindowKeyDown = window.onkeydown;
+
+    //Sweet alert to confirm deletion of client
+    swal({
+      title: "Terms and Conditions",
+      text: "You will be prompted for payment provided by Stripe. All payments are securely stored and handled through the Stripe website.  By clicking Accept, you will agree to these terms and will proceed to make your payment with FitMe.",
+      type: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#DD6B55",
+      confirmButtonText: "Accept",
+      closeOnConfirm: true
+    }, function (isConfirm) {
+      window.onkeydown = previousWindowKeyDown;
+      if (isConfirm) {
+        template.processing.set(true);
+        template.checkout.open({
+          email: currentTrainer.emails[0].address,
+          name: '20 Additional Clients',
+          description: "Add twenty additional clients to your client limit",
+          amount: 1300,
+          bitcoin: true
+        });
+
+        planType = "Twenty Additional Clients";
+        dollarAmount = 1300;
+
+      } else {
+        swal('Cancelled', 'You Will Not Be Charged.', 'error');
+      }
+    });
+  },
 });
---
+++ server/methods/trainers/myAccount/twentyAdditionalClientsMethod.js
@@ -0,0 +1,29 @@
+Meteor.methods({
+  twentyAdditionalClientsTrainer(trainerId) {
+    if (Roles.userIsInRole(this.userId, "trainer")) {
+      let curTrainer = Meteor.users.findOne({
+        _id: trainerId
+      });
+
+      //Prevent client side console upgrading plans if they have not paid
+      if (curTrainer.hasPaid == false) {
+        throw new Meteor.Error("You must make a payment first");
+      }
+      
+      //Check if the user is suspended before letting them update their client limit
+      if(curTrainer.userStatus == "suspended") {
+        throw new Meteor.Error("You must activate your account first");
+      }
+
+      Meteor.users.update({
+        _id: trainerId
+      }, {
+        $inc: {
+          clientLimit: 20,
+        }
+      });
+    } else {
+      throw new Meteor.Error("not-authorized");
+    }
+  }
+});
