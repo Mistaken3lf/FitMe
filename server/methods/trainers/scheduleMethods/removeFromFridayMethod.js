a461333718378216674d2edb0dc06ae4cf8ddd4a
Tyler <mistaken3lf@gmail.com>
2015-12-30 17:26:38 +0000

    Removed email cancellation for appointments for now

--- server/methods/trainers/scheduleMethods/removeFromFridayMethod.js
+++ server/methods/trainers/scheduleMethods/removeFromFridayMethod.js
@@ -13,13 +13,6 @@ Meteor.methods({
         _id: this.userId
       });
 
-      const trainersClient = Meteor.users.findOne({
-        _id: fridaysItem
-      });
-
-      const trainersEmail = thisTrainer.emails[0].address;
-      const clientsEmail = trainersClient.emails[0].address;
-
       if (thisTrainer.userStatus == "suspended") {
         throw new Meteor.Error("Sorry, your account has been suspended");
       }
@@ -33,16 +26,6 @@ Meteor.methods({
           fridayDescription: "",
           fridayStatus: false
         }
-      });
-
-      this.unblock();
-
-      //Send the actual email to us
-      Email.send({
-        to: clientsEmail,
-        from: trainersEmail,
-        subject: "FitMe -- Appointment Cancellation",
-        text: "Hello " + trainersClient.firstName + " " + trainersClient.lastName + ',\n\n' + "We wanted to inform you that, your trainer, " + thisTrainer.firstName + " " + thisTrainer.lastName + " has cancelled their appointment for friday"
       });
 
     } else {
