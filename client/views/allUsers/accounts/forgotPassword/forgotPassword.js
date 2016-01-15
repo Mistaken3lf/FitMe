132cc6b2b59072412e9525154282e168e8779105
Tyler <mistaken3lf@gmail.com>
2016-01-15 00:13:12 +0000

    Editor test

--- client/views/allUsers/accounts/forgotPassword/forgotPassword.js
+++ client/views/allUsers/accounts/forgotPassword/forgotPassword.js
@@ -59,20 +59,20 @@ Template.resetPassword.events({
         Bert.alert(error, 'danger', 'growl-top-right');
       } else {
         Bert.alert("Password successfully changed", 'success', 'growl-top-right');
-        
+
         //Unset the reset password token
         Session.set('resetPassword', null);
-        
+
         //Go to the admins home if they are an admin
         if(Roles.userIsInRole(Meteor.userId(), "admin")) {
           FlowRouter.go("/adminHome");
         }
-        
+
         //Go to the trainer home if they are a trainer
         if(Roles.userIsInRole(Meteor.userId(), "trainer")) {
           FlowRouter.go("/trainerHome");
         }
-        
+
         //Go to the clients home if they are a client
         if(Roles.userIsInRole(Meteor.userId(), "client")) {
           FlowRouter.go("/clientHome");
