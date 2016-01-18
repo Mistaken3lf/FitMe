32179e1ed513abf14fd1671db099ea0c4d52f647
Tyler <mistaken3lf@gmail.com>
2016-01-18 15:58:54 +0000

    Fixed payment message to use html tags

--- server/methods/admin/accountSettings/paymentDueSoonMethod.js
+++ server/methods/admin/accountSettings/paymentDueSoonMethod.js
@@ -4,28 +4,25 @@ Meteor.methods({
         trainerId: {
           type: String
         },
-        
+
         expiresOn: {
           type: String
         }
       }).validate({
         trainerId, expiresOn
       });
-    
+
     if (Roles.userIsInRole(this.userId, "admin")) {
       const trainer = Meteor.users.findOne({
         _id: trainerId
       });
-      
+
       //Get trainers email address
-      let emailAddr = trainer.emails[0].address;
-      
+      const emailAddr = trainer.emails[0].address;
+
       //Account page link
-      let fitmeLink = "www.gofitme.com/myAccount";
-      
-      //Bold the fitme team text
-      let fitmeTeam = "The FitMe Team";
-      
+      const fitmeLink = "www.gofitme.com/myAccount";
+
       //Allow other methods to execute without blocking for email
       this.unblock();
 
@@ -34,7 +31,7 @@ Meteor.methods({
         to: emailAddr,
         from: "sales@gofitme.com",
         subject: "FitMe -- Account Renewal",
-        text: "Hello " + trainer.firstName + " " + trainer.lastName + ',\n\n' + "We wanted to inform you that your account will be expiring on " + expiresOn + ". " + "To renew your account, visit the following link " + fitmeLink + "." + " If you fail to renew, your account may be briefly suspended until you renew it.\n\n\n" + "Keep Training Hard,\n\n" + fitmeTeam
+        html: "<p>Hello " + trainer.firstName + " " + trainer.lastName + "<br><br>" + "We wanted to inform you that your account will be expiring on " + expiresOn + ". " + "To renew your account, visit the following link " + fitmeLink + "." + " If you fail to renew, your account may be briefly suspended until you renew it.<br><br><br>" + "Keep Training Hard,<br><br>" + "<strong>The FitMe Team</strong></p>"
       });
     } else {
       throw new Meteor.error("Not Authorized");
