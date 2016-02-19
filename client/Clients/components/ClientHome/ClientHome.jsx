2b3618cab816f8ee6168ce7bebcdc8a834d1a2f6
Tyler <mistaken3lf@gmail.com>
2016-02-19 14:59:12 +0000

    Client home is now a react component

---
+++ client/Clients/components/ClientHome/ClientHome.jsx
@@ -0,0 +1,21 @@
+ClientHome = React.createClass({
+  render() {
+    if(Roles.userIsInRole(Meteor.userId(), "client")) {
+      return(
+        <div>
+          <ClientHomeHeader />
+          <ClientHomeWelcome />
+          <ClientHomeFooter />
+        </div>
+      );
+    } else if(Meteor.loggingIn()) {
+        return (
+          <Loading />
+        );
+    } else {
+      return (
+        <NotAuthorized />
+      );
+    }
+  }
+})
--- client/Clients/components/ClientHome/clientHome.js
+++ client/Clients/components/ClientHome/clientHome.js
@@ -1,42 +1,5 @@
-Template.clientHome.onRendered(() => {
-  //Initialization for the parallax images
-  $('.parallax').parallax();
-});
-
-Template.clientHome.events({
-  'submit form' (event) {
-    event.preventDefault();
-    //Get who send the message and the actual message
-    let firstName = $('[name=firstName]').val();
-    let lastName = $('[name=lastName]').val();
-    let phoneNumber = $('[name=phoneNumber]').val();
-    let email = $('[name=email]').val();
-    let message = $('[name=message]').val();
-    //Call the send feedback method to send us information.
-    Meteor.call("contactUs", firstName, lastName, phoneNumber, email, message, (error) => {
-      if (error) {
-        Bert.alert(error.reason, 'danger', 'growl-top-right');
-      } else {
-        //Show success message to let them know the email sent
-        Bert.alert("Email successfully sent", 'success', 'growl-top-right');
-
-        //Reset the form values to blank
-        firstName = $('[name=firstName]').val('');
-        lastName = $('[name=lastName]').val('');
-        phoneNumber = $('[name=phoneNumber]').val('');
-        email = $('[name=email]').val('');
-        message = $('[name=message]').val('');
-        
-        //Go back to client home
-        FlowRouter.go("/clientHome");
-      }
-    });
-  }
-});
-
 Template.clientHome.helpers({
-  //Check if the user is logging in 
-  isLoggingIn() {
-    return Meteor.loggingIn();
+  ClientHome() {
+    return ClientHome;
   }
 });
