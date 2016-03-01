9df601357d43a95f6d38856c5ab9f19f6f187a33
Tyler <mistaken3lf@gmail.com>
2016-03-01 01:31:10 +0000

    New not found page

--- client/Common/components/NotFound/NotFound.jsx
+++ client/Common/components/NotFound/NotFound.jsx
@@ -1,13 +1,13 @@
 NotFound = React.createClass({
   render() {
+    const styles = {
+      imageStyle: {
+        height: "100vh"
+      }
+    };
+
     return (
-      <div className="row">
-        <div className="col s12 m8 offset-m2 l6 offset-l3" id="userLoginForm">
-          <div className="card-panel green z-depth-2">
-            <h1 className="white-text">Shhhh, This Place Does Not Even Exist</h1>
-          </div>
-        </div>
-      </div>
+      <img src="/Home/lost2.jpg" style={styles.imageStyle} className="responsive-img" />
     );
   }
 });
