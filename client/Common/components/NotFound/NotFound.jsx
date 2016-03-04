28879018c3a4a7435f82df5cf58dd62cc3f0468b
Tyler <mistaken3lf@gmail.com>
2016-03-04 22:03:28 +0000

    New not found component

--- client/Common/components/NotFound/NotFound.jsx
+++ client/Common/components/NotFound/NotFound.jsx
@@ -1,13 +1,24 @@
 NotFound = React.createClass({
   render() {
-    const styles = {
-      imageStyle: {
-        height: "100vh"
-      }
-    };
-
     return (
-      <img src="/Home/lost2.jpg" style={styles.imageStyle} className="responsive-img" />
+      <div className="row">
+        <div className="col s12 m8 offset-m2 l6 offset-l3" id="userLoginForm">
+          <div className="card-panel green z-depth-2">
+            <div className="row">
+              <div className="col s12 center">
+                <i className="white-text large material-icons">assignment_late</i>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col s12 center-align">
+               <h5 className="white-text">The page your are trying to view does not exist.
+               please check that you have spelled everything correctly. If you are still
+               having problems please contact FitMe, Thanks.</h5>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     );
   }
 });
