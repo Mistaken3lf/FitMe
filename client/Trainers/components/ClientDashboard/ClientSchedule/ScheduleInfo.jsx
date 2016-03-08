388fa3fc59a871879f769c517ed84f6d6f115f5c
Tyler <mistaken3lf@gmail.com>
2016-03-08 15:53:43 +0000

    Alignment fixes

--- client/Trainers/components/ClientDashboard/ClientSchedule/ScheduleInfo.jsx
+++ client/Trainers/components/ClientDashboard/ClientSchedule/ScheduleInfo.jsx
@@ -23,16 +23,16 @@ ScheduleInfo = React.createClass({
       <div className="col s12 m12 l12">
         <div className="card white z-depth-1">
           <div className="row">
-            <div className="col s12 m4 l4">
-              <h6 className="center-align blue-text"><b>Sessions Remaining</b></h6>
-              <input type="number" ref="sessionsRemaining" name="sessionsRemaining" className="center-align" defaultValue={this.props.scheduleData.sessionsRemaining} onChange={this.updateField} placeholder="Ex. 20" />
+            <div className="col s12 m4 l4 center-align">
+              <h6 className="blue-text"><b>Sessions Remaining</b></h6>
+              <input type="number" ref="sessionsRemaining" className="center-align" name="sessionsRemaining" defaultValue={this.props.scheduleData.sessionsRemaining} onChange={this.updateField} placeholder="Ex. 20" />
             </div>
-            <div className="col s12 m4 l4">
-              <h6 className="center-align blue-text"><b>Payment Due</b></h6>
+            <div className="col s12 m4 l4 center-align">
+              <h6 className="blue-text"><b>Payment Due</b></h6>
               <input type="date" ref="paymentDue" className="center-align" name="paymentDue" defaultValue={this.props.scheduleData.paymentDue} onChange={this.updateField} placeholder="Ex. 05/29/1990" />
             </div>
-            <div className="col s12 m4 l4 center">
-              <h6 className="center-align blue-text"><b>Today's Date</b></h6>
+            <div className="col s12 m4 l4 center-align">
+              <h6 className="blue-text center-align"><b>Today's Date</b></h6>
               <br />
               <span>{this.todaysDate()}</span>
             </div>
