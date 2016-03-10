fa1004fc928e6273e13c45bdc1330bb9614ad3dc
Tyler <mistaken3lf@gmail.com>
2016-03-10 15:49:59 +0000

    Speed up all user admin publication

--- server/publications/Admin/AllUsers.js
+++ server/publications/Admin/AllUsers.js
@@ -2,7 +2,20 @@ Meteor.publish('allUsers', function () {
   //If the user is an admin and logged in
   if (Roles.userIsInRole(this.userId, "admin")) {
     //Find all my users
-    return Meteor.users.find();
+    return Meteor.users.find({}, {
+      fields: {
+        username: 1,
+        "emails.address": 1,
+        firstName: 1,
+        lastName: 1,
+        userStatus: 1,
+        planType: 1,
+        expiresOn: 1,
+        clientLimit: 1,
+        roles: 1,
+        datePurchased: 1
+      }
+    });
   }
 
   //User is not authorized to access this publication
