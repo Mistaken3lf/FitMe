d5a8c50b5e9e72e1441503071e92b400afcbbe20
Tyler <mistaken3lf@gmail.com>
2016-03-18 14:30:04 +0000

    Removed validate class

--- client/Clients/components/MyDashboard/MyTrainer/MyTrainer.jsx
+++ client/Clients/components/MyDashboard/MyTrainer/MyTrainer.jsx
@@ -44,71 +44,71 @@ MyTrainer = React.createClass({
                     <div className="row">
                       <div className="input-field col s12 m6 l6">
                         <span className="blue-text myBoldProfileHeading">Username:</span>
-                        <input type="text" name="username" className="validate" placeholder="Username" defaultValue={this.data.myTrainer.username} readOnly />
+                        <input type="text" name="username" placeholder="Username" defaultValue={this.data.myTrainer.username} readOnly />
                       </div>
                     </div>
                     <div className="row">
                       <div className="input-field col s12 m6 l6">
                         <span className="blue-text myBoldProfileHeading">First Name:</span>
-                        <input type="text" name="firstName" className="validate" placeholder="First Name" minLength={2} defaultValue={this.data.myTrainer.firstName} readOnly />
+                        <input type="text" name="firstName" placeholder="First Name" minLength={2} defaultValue={this.data.myTrainer.firstName} readOnly />
                       </div>
                       <div className="input-field col s12 m6 l6">
                         <span className="blue-text myBoldProfileHeading">Last Name:</span>
-                        <input type="text" name="lastName" className="validate" placeholder="Last Name" minLength={2} defaultValue={this.data.myTrainer.lastName} readOnly />
+                        <input type="text" name="lastName" placeholder="Last Name" minLength={2} defaultValue={this.data.myTrainer.lastName} readOnly />
                       </div>
                     </div>
                     <div className="row">
                       <div className="input-field col s12 m6 l6">
                         <span className="blue-text myBoldProfileHeading">Date Of Birth:</span>
-                        <input type="date" name="birthday" className="validate" id="birthday" placeholder="Date of Birth" defaultValue={this.data.myTrainer.birthday} readOnly />
+                        <input type="date" name="birthday" id="birthday" placeholder="Date of Birth" defaultValue={this.data.myTrainer.birthday} readOnly />
                       </div>
                     </div>
                     <div className="row">
                       <div className="input-field col s12 m3 l3">
                         <span className="blue-text myBoldProfileHeading">Address:</span>
-                        <input type="text" name="address" className="validate" placeholder="Address" defaultValue={this.data.myTrainer.address} readOnly />
+                        <input type="text" name="address" placeholder="Address" defaultValue={this.data.myTrainer.address} readOnly />
                       </div>
                       <div className="input-field col s12 m3 l3">
                         <span className="blue-text myBoldProfileHeading">City:</span>
-                        <input type="text" name="city" className="validate" placeholder="City" defaultValue={this.data.myTrainer.city} readOnly />
+                        <input type="text" name="city" placeholder="City" defaultValue={this.data.myTrainer.city} readOnly />
                       </div>
                       <div className="input-field col s12 m3 l3">
                         <span className="blue-text myBoldProfileHeading">State:</span>
-                        <input type="text" name="state" className="validate" placeholder="State" defaultValue={this.data.myTrainer.state} readOnly />
+                        <input type="text" name="state" placeholder="State" defaultValue={this.data.myTrainer.state} readOnly />
                       </div>
                       <div className="input-field col s12 m3 l3">
                         <span className="blue-text myBoldProfileHeading">Zip Code</span>
-                        <input type="text" name="zip" className="validate" placeholder="Zip Code" defaultValue={this.data.myTrainer.zip} readOnly />
+                        <input type="text" name="zip" placeholder="Zip Code" defaultValue={this.data.myTrainer.zip} readOnly />
                       </div>
                     </div>
                     <div className="row">
                       <div className="input-field col s12 m4 l4">
                         <span className="blue-text myBoldProfileHeading">Home Phone:</span>
-                        <input type="text" name="homePhone" className="validate" placeholder="Home Phone" defaultValue={this.data.myTrainer.homePhone} readOnly />
+                        <input type="text" name="homePhone" placeholder="Home Phone" defaultValue={this.data.myTrainer.homePhone} readOnly />
                       </div>
                       <div className="input-field col s12 m4 l4">
                         <span className="blue-text myBoldProfileHeading">Work Phone:</span>
-                        <input type="text" name="workPhone" className="validate" placeholder="Work Phone" defaultValue={this.data.myTrainer.workPhone} readOnly />
+                        <input type="text" name="workPhone" placeholder="Work Phone" defaultValue={this.data.myTrainer.workPhone} readOnly />
                       </div>
                       <div className="input-field col s12 m4 l4">
                         <span className="blue-text myBoldProfileHeading">Emergency Contact:</span>
-                        <input type="text" name="emergencyContact" className="validate" placeholder="Emergency Contact" defaultValue={this.data.myTrainer.emergencyContact} readOnly />
+                        <input type="text" name="emergencyContact" placeholder="Emergency Contact" defaultValue={this.data.myTrainer.emergencyContact} readOnly />
                       </div>
                     </div>
                     <div className="row">
                       <div className="input-field col s12 m6 l6">
                         <span className="blue-text myBoldProfileHeading">Email:</span>
-                        <input type="text" name="email" className="validate" placeholder="Email" defaultValue={this.data.myTrainer.emails[0].address} readOnly />
+                        <input type="text" name="email" placeholder="Email" defaultValue={this.data.myTrainer.emails[0].address} readOnly />
                       </div>
                     </div>
                     <div className="row">
                       <div className="input-field col s12 m6 l6">
                         <span className="blue-text myBoldProfileHeading">About You:</span>
-                        <textarea name="bio" placeholder="About You" className="validate" className="materialize-textarea" rows={6} defaultValue={this.data.myTrainer.bio} readOnly></textarea>
+                        <textarea name="bio" placeholder="About You" className="materialize-textarea" rows={6} defaultValue={this.data.myTrainer.bio} readOnly></textarea>
                       </div>
                       <div className="input-field col s12 m6 l6">
                         <span className="blue-text myBoldProfileHeading">Fitness Goals:</span>
-                        <textarea name="fitnessGoals" className="validate" placeholder="Fitness Goals" className="materialize-textarea" rows={6} defaultValue={this.data.myTrainer.fitnessGoals} readOnly></textarea>
+                        <textarea name="fitnessGoals" placeholder="Fitness Goals" className="materialize-textarea" rows={6} defaultValue={this.data.myTrainer.fitnessGoals} readOnly></textarea>
                       </div>
                     </div>
                   </div>
