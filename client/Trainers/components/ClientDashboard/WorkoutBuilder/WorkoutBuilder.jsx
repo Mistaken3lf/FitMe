21ff92fe77835f14bedef573a8db701f72f86c3f
Tyler <mistaken3lf@gmail.com>
2016-03-22 14:56:24 +0000

    Started workout builder layout

--- client/Trainers/components/ClientDashboard/WorkoutBuilder/WorkoutBuilder.jsx
+++ client/Trainers/components/ClientDashboard/WorkoutBuilder/WorkoutBuilder.jsx
@@ -1,8 +1,14 @@
 WorkoutBuilder = React.createClass({
   render() {
     return (
-      <div>
-        <h1>Workout Builder</h1>
+      <div className="card grey lighten-4">
+        <div className="col s12 m12 l12">
+          <div className="card z-depth-1">
+            <div className="blue card-title center-align white-text">WORKOUT BUILDER</div>
+          </div>
+        </div>
+        <WorkoutDetails />
+        <CardioRoutine />
       </div>
     );
   }
