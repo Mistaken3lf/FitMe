a636b852ad0df86ce76f1f7cdcbdb6ed6332a9af
Tyler <mistaken3lf@gmail.com>
2016-03-29 15:38:14 +0000

    Removed workout builder

--- client/Trainers/components/ClientDashboard/WorkoutBuilder/SaveWorkoutButton.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-SaveWorkoutButton = React.createClass({
-  render() {
-    return (
-      <button className="btn green waves-effect white-text right">Save Workout</button>
-    );
-  }
-});
