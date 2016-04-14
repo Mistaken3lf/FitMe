292352222104006254fc446853ad13a82887a2cc
mcphersonad21 <anthony.d.mcpherson@gmail.com>
2016-04-14 02:21:24 +0000

    Removed learn more button from trainer home

--- client/trainers/components/TrainerHome/TrainerHomeHeader.js
+++ client/trainers/components/TrainerHome/TrainerHomeHeader.js
@@ -1,16 +1,5 @@
 import React from 'react';
 
-function learnMore() {
-  swal({
-    title: "Free Account!",
-    html: true,
-    text: "Everyone signing up for FitMe will be able to a create free account which includes a free 7-day trial.  Users will also be able to \
-          create one <b>FREE</b> client so they can test out FitMe.  After your free 7-day trial account expires, you can either cancel your account or proceed \
-          in purchasing a paid plan.",
-    imageUrl: "navigation/fitMeSidebarLogo.png"
-  });
-}
-
 const TrainerHomeHeader = () => (
   <section id="parallax-top">
     <div id="hero">
@@ -30,11 +19,9 @@ const TrainerHomeHeader = () => (
       <br />
       <br />
       <br />
-      <h5 className="header col s12 light white-text">Sign up for FREE today! The first client is on us.</h5>
       <br />
       <br />
       <br />
-      <button className="waves-effect waves-light btn blue center" onClick={this.learnMore}>Learn More</button>
     </div>
     </div>
   </section>
