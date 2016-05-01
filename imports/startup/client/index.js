fbff7070fd75a70598cdc81cc92644921c3fce5b
Tyler <mistaken3lf@gmail.com>
2016-05-01 17:53:46 +0000

    More structuring

--- imports/startup/client/index.js
+++ imports/startup/client/index.js
@@ -1,7 +1,8 @@
-import '../node_modules/react-s-alert/dist/s-alert-default.css';
-import '../node_modules/react-s-alert/dist/s-alert-css-effects/jelly.css';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import '../node_modules/bootstrap/dist/js/bootstrap.min.js';
+import '/node_modules/react-s-alert/dist/s-alert-default.css';
+import '/node_modules/react-s-alert/dist/s-alert-css-effects/jelly.css';
+import '/node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '/node_modules/bootstrap/dist/js/bootstrap.min.js';
 import './resetPassword.js';
 import './routes.js';
+import './onLogin.js';
 
--- imports/ui/registration/Registration.js
+++ imports/ui/registration/Registration.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Alert from 'react-s-alert';
 
-export default class Register extends React.Component {
+export default class Registration extends React.Component {
   handleSubmit(e) {
     e.preventDefault();
 
