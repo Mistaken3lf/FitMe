f0641c97e8eccd1c5a5f28ae7202e7abf642c62b
knipferrc <knipferrc@gmail.com>
2016-08-14 13:50:09 -0500

    FitMe 1.2 Released!!!

--- client/views/common/components/Navigation/Navigation.js
+++ client/views/common/components/Navigation/Navigation.js
@@ -7,7 +7,7 @@ import CommonNavigation from './CommonNavigation.js';
 
 export default class Navigation extends React.Component {
   versionNumber() {
-    const version = "1.1.9";
+    const version = "1.2";
 
     return version;
   }
